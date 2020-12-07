2e95d24a125568eaafe1917c45a66d92b6ead985
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2020-12-08 00:45:34 +0100

    Code refactor

--- lekcja_11/src/components/User/User.js
+++ lekcja_11/src/components/User/User.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import { dateFormatter } from "../../utils/dateFormatter";
-import ReactPlaceholder from 'react-placeholder';
+import ReactPlaceholder from "react-placeholder";
 import "react-placeholder/lib/reactPlaceholder.css";
 export default function User() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-  const sendData = (data)=> {
-    localStorage.setItem('userdata', JSON.stringify(data));
-   }
+  const sendData = (data) => {
+    localStorage.setItem("userdata", JSON.stringify(data));
+  };
   useEffect(() => {
     let itemUrl = `https://randomuser.me/api/`;
     let data;
@@ -23,38 +23,37 @@ export default function User() {
       });
   }, {});
   return (
-    <ReactPlaceholder type='media' rows={5} ready={!isLoading}>
-    {!isLoading?
-    <div className="User">
-    <div>
-      <img
-        src={data.picture.medium}
-        alt={`${data.name.first} ${data.name.last}`}
-      />
-      <h1>
-        <button
-          onClick={() => {
-            sendData(data)
-          }}
-        >
-          <Link to={`/users/${data.login.uuid}`}>
-            {data.name.first && data.name.last
-              ? `${data.name.first} ${data.name.last}`
-              : "Nie przekazano imienia i nazwiska."}
-          </Link>
-        </button>
-      </h1>
-      <p>
-        {data.location.street.name && data.location.city
-          ? `${data.location.street.name}, ${data.location.city}`
-          : "brak"}
-      </p>
-      <p>{data.email}</p>
-      <p>{dateFormatter(data.registered.date)}</p>
-    </div>
-</div>:null}
-    
-  
+    <ReactPlaceholder type="media" rows={5} ready={!isLoading}>
+      {!isLoading ? (
+        <div className="User">
+          <div>
+            <img
+              src={data.picture.medium}
+              alt={`${data.name.first} ${data.name.last}`}
+            />
+            <h1>
+              <button
+                onClick={() => {
+                  sendData(data);
+                }}
+              >
+                <Link to={`/users/${data.login.uuid}`}>
+                  {data.name.first && data.name.last
+                    ? `${data.name.first} ${data.name.last}`
+                    : "Nie przekazano imienia i nazwiska."}
+                </Link>
+              </button>
+            </h1>
+            <p>
+              {data.location.street.name && data.location.city
+                ? `${data.location.street.name}, ${data.location.city}`
+                : "brak"}
+            </p>
+            <p>{data.email}</p>
+            <p>{dateFormatter(data.registered.date)}</p>
+          </div>
+        </div>
+      ) : null}
     </ReactPlaceholder>
   );
 }
--- lekcja_11/src/components/UserDetails/UserDetails.js
+++ lekcja_11/src/components/UserDetails/UserDetails.js
@@ -1,9 +1,61 @@
-import React from "react";
-import User2 from '../User2/User2'
+import React, { useState, useEffect } from "react";
+import { dateFormatter } from "../../utils/dateFormatter";
+import ReactPlaceholder from "react-placeholder";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "react-placeholder/lib/reactPlaceholder.css";
+
 export default function UserDetails() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(function () {
+      let cachedData = JSON.parse(localStorage.getItem("userdata"));
+      setData(cachedData);
+      setIsLoading(false);
+    }, 1000);
+  }, {});
+
   return (
-    <div className="User">
-      <User2/>
-    </div>
+    <ReactPlaceholder type="media" rows={5} ready={!isLoading}>
+      {!isLoading ? (
+        <div className="User">
+          <div>
+            <img
+              src={data.picture.medium}
+              alt={`${data.name.first} ${data.name.last}`}
+            />
+            <h1>
+              {data.name.first && data.name.last
+                ? `${data.name.first} ${data.name.last}`
+                : "Nie przekazano imienia i nazwiska."}
+            </h1>
+            <p>
+              {data.location.street.name && data.location.city
+                ? `${data.location.street.name}, ${data.location.city}`
+                : "brak"}
+            </p>
+            <p>{data.email}</p>
+            <p>{dateFormatter(data.registered.date)}</p>
+            <MapContainer
+              className="map"
+              center={[data.location.coordinates.latitude,data.location.coordinates.longitude]}
+              zoom={13}
+              scrollWheelZoom={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[data.location.coordinates.latitude,data.location.coordinates.longitude]}>
+                <Popup>
+                  {`Localization of ${data.name.first} ${data.name.last}`}
+                </Popup>
+              </Marker>
+            </MapContainer>
+            ,
+          </div>
+        </div>
+      ) : null}
+    </ReactPlaceholder>
   );
 }
